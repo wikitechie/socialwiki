ec48342038772ffe15580ce193b1b1715c2c8092
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-08-20 20:41:27 +0300

    fixed a bug ( title is not meta !! )

--- pages/wikis/recentchanges.php
+++ pages/wikis/recentchanges.php
@@ -37,13 +37,14 @@
 			       'subtype'=>'wikiuser'
 			,
 			       'metadata_name_value_pairs' => array(
-						array('matadata_name' => 'wikiuser_name','matadata_value' => $recentchange['user']),
-						array('matadata_name' => 'wiki_id','matadata_value' => $wiki->title)
+						array('name' => 'wikiuser_name','value' => $recentchange['user']),
+						array('name' => 'wiki_id','value' => $wiki->title)
 					),
 			       'metadata_name_value_pairs_operator' => 'AND'
 			) ;
 			$results = elgg_get_entities_from_metadata($options);
-			$check_user = count($results); //to be replaced with checking user code
+
+			$check_user = count($results); 
 			
 			if($check_user){
 				$content .= "<p>".print_r($recentchange, true)."</p>";
