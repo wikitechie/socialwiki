767cdca2f3cc04cd42467de6fda8d7947b6f5f58
alAmjadTawfiq Isstaif <isstaif@gmail.com>
2011-08-23 00:48:38 +0300

    wiki view cleanup

--- pages/wikis/all.php
+++ pages/wikis/all.php
@@ -1,14 +1,21 @@
 <?php
 	
-    $body = elgg_view('output/longtext', array('value'=>'Here you can find a complete list of wikis on our website!'));
-	
+    $body = elgg_view_title("Wikis");
+		
 	$body .= elgg_view('output/url', array('text'=>'Add a new wiki!', 'href'=>'/socialwiki/wikis/add'));
 					
 	$body .= elgg_list_entities(array(
 	    'type' => 'object',
 	    'subtype' => 'wiki',
 	));
-	$body = elgg_view_layout('one_column', $body);
+	
+	elgg_pop_breadcrumb();
+	elgg_push_breadcrumb("Wikis");
+	
+	//elgg_register_title_button('socialwiki/wikis/add','My button');
+	
+	
+	$body = elgg_view_layout('two_column_left_sidebar', '', $body);
  
-	echo elgg_view_page("List of supported wikis!", $body);
+	echo elgg_view_page("All wikis", $body);
 ?>
