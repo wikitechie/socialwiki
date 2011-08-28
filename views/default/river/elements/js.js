8b8f7d85396b7e0ddfa4d27629f5f2524e793f1e
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-08-28 11:59:31 +0300

    Fixed toggling all diff links' text

--- views/default/river/elements/js.js
+++ views/default/river/elements/js.js
@@ -1,9 +1,10 @@
 $(window).ready(function() {
 	$("a[href='#toggleDiff']").click(function() {
 		var container = $(this).parent().parent().parent().find("div.sliding-extender");
-		$nextname = $(this).html(); 
-		$(this).html($(this).attr('data-nextname'));
-		$(this).attr('data-nextname',$nextname);
+		all_links = $("a[href='#toggleDiff']"); // to get all links not this only
+		$nextname = all_links.html(); 
+		all_links.html($(this).attr('data-nextname'));
+		all_links.attr('data-nextname',$nextname);
 		container.slideToggle('slow');
 		
 	});
