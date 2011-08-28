f38114d11bf47d36d773164e346733c3b7715f64
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-08-28 11:02:59 +0300

    Optimized diff Show/Hide links text

--- views/default/river/elements/js.js
+++ views/default/river/elements/js.js
@@ -1,10 +1,9 @@
 $(window).ready(function() {
-	$("a[href='#showdiff']").click(function() {
+	$("a[href='#toggleDiff']").click(function() {
 		var container = $(this).parent().parent().parent().find("div.attachments");
-		if (! $(container).is(":hidden"))
-			$(this).html('View difference');
-		else
-			$(this).html('Hide difference');
+		$nextname = $(this).html(); 
+		$(this).html($(this).attr('data-nextname'));
+		$(this).attr('data-nextname',$nextname);
 		container.slideToggle('slow');
 		
 	});
