a4b90fdde185cd03f306db5be5a137a21529ff73
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-08-28 12:16:10 +0300

    Fixed a conflict on toggling links text

--- views/default/river/elements/js.js
+++ views/default/river/elements/js.js
@@ -1,7 +1,8 @@
 $(window).ready(function() {
 	$("a[href='#toggleDiff']").click(function() {
-		var container = $(this).parent().parent().parent().find("div.sliding-extender");
-		all_links = $("a[href='#toggleDiff']"); // to get all links not this only
+		var riverItem = $(this).parent().parent().parent();
+		var container = riverItem.find("div.sliding-extender");
+		all_links = riverItem.find("a[href='#toggleDiff']"); // to get all links not this only
 		$nextname = all_links.html(); 
 		all_links.html($(this).attr('data-nextname'));
 		all_links.attr('data-nextname',$nextname);
