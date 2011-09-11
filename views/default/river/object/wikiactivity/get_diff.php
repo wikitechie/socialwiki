2e2505a6454d36c3f27fa2d6d77623b936fc4000
Muhammad Al-Syrwan <MhdSyrwan@gmail.com>
2011-09-12 02:47:35 +0300

    Added a view to get wiki diff

---
+++ views/default/river/object/wikiactivity/get_diff.php
@@ -0,0 +1,14 @@
+<?php
+echo "asd";
+	$id = (int)$vars['id'];
+	/** @var ElggRiverItem */
+	$item = elgg_get_river(array('ids'=>$id));
+	$item = $item[0];
+	/** @var ElggObject */
+	$wikiactivity = $item->getObjectEntity();
+	if ($wikiactivity->diff == "") {
+		sw_update_wiki_diff($wikiactivity);
+	}
+	echo $wikiactivity->diff;
+	
+?>
